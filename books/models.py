f8266a71d83df18a102f076875621c0bd9a282cb
Jeong Taek <imjt9345@naver.com>
2023-09-04 21:59:04 +0900

    Book, Rental 모델 추가, 마이그레이션

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    stock = models.PositiveIntegerField()
+    summary = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Rental(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='books')
+    rental_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField()
+
+    class Meta:
+        ordering = ['rental_date']

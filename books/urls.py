15b3d354ddf6be5918106d4f8c5307bb3b5f5a73
Jeong Taek <imjt9345@naver.com>
2023-09-05 00:00:39 +0900

    book_list, book_detail 뷰 함수, url, 템플릿 추가

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from books import views
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('books/<book_id>/', views.book_detail, name='book_detail'),
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from books.models import Book
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'books/book_detail.html', {'book': book})
+
+
+

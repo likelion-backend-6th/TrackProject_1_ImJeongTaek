5555491813decc8731c8229d7bbedc22369a9f82
Jeong Taek <imjt9345@naver.com>
2023-09-04 22:12:49 +0900

    Book, Rental admin 추가

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book, Rental
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publisher', 'stock']
+
+
+@admin.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ['user', 'get_book_title', 'rental_date', 'return_date']
+
+    def get_book_title(self, obj):
+        return obj.book.title
+
+    get_book_title.short_description = 'Book Title'

e1cce57de343968a230d610f651f781005ed70ed
Jeong Taek <imjt9345@naver.com>
2023-09-05 21:50:29 +0900

    CustomLoginView 클래스 추가, url 설정 추가

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from account import views
+
+urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('', include('django.contrib.auth.urls')),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('books:book_list')
+        return super().dispatch(request, *args, **kwargs)

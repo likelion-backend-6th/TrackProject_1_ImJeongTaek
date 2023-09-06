c273d6820b130e73502e3c5cc7831410d1939513
Jeong Taek <imjt9345@naver.com>
2023-09-06 21:59:18 +0900

    rent_book url, login_required 설정

--- books/urls.py
+++ books/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('rental_list/', views.rental_list, name='rental_list'),
     path('books/<book_id>/', views.book_detail, name='book_detail'),
+    path('books/<book_id>/rent/', views.rent_book, name='rent_book'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from books.models import Book, Rental
@@ -14,6 +15,7 @@ def book_detail(request, book_id):
     return render(request, 'books/book_detail.html', {'book': book})
 
 
+@login_required
 def rent_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
 
@@ -26,15 +28,6 @@ def rent_book(request, book_id):
     else:
         messages.error(request, f'{book.title} is no stock.')
         return redirect('books:book_list')
-
-
-def return_book(request, rental_id):
-    rental = Rental(user=request.user, id=rental_id)
-    book = rental.book
-
-    book.stock += 1
-    book.save()
-    return redirect('books:book_list')
 
 
 def rental_list(request):

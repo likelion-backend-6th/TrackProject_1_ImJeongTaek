9b79c71fae62d670087a245cd350a98c5de2944f
Jeong Taek <imjt9345@naver.com>
2023-09-06 22:06:20 +0900

    admin 페이지에서 책 반납하는 기능 추가

--- books/admin.py
+++ books/admin.py
@@ -11,8 +11,19 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(Rental)
 class RentalAdmin(admin.ModelAdmin):
     list_display = ['user', 'get_book_title', 'rental_date', 'return_date']
+    actions = ['return_book_action']
 
     def get_book_title(self, obj):
         return obj.book.title
 
     get_book_title.short_description = 'Book Title'
+
+    def return_book_action(self, request, queryset):
+        for rental in queryset:
+            rental.book.stock += 1
+            rental.book.save()
+            rental.delete()
+
+        self.message_user(request, f'Successfully returned {len(queryset)} rentals.')
+
+    return_book_action.short_description = 'Return selected rentals'
